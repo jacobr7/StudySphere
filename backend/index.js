43b838a6902e56eb917ed47e67d92bcdbf39c93d
nicholas seah <nicholasseah02@gmail.com>
2024-11-10 00:32:54 +0800

    Changes to allow .env in vercel

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors'); // Import cors
 const multer = require('multer')
+require('dotenv').config();
 const mongoose = require('./db'); // Import the MongoDB connection
 const FileModel = require('./models/FileModel'); // Import the Note model
 const axios = require('axios'); // Make sure axios is imported
 
 const admin = require("firebase-admin");//Import firebase admin SDK
-const serviceAccount = require("./config/firebase-admin.json"); // Import the service account key
+// const serviceAccount = require("./config/firebase-admin.json"); // Import the service account key
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri":  process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL,
+  "universe_domain": "googleapis.com"
+}
+
 
 const app = express();
 const PORT = 5000;
